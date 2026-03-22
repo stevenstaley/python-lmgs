bc390ba0a51d175b5fe912c4b645be59fa468eff
stevenstaley <73314563+stevenstaley@users.noreply.github.com>
2026-03-22 14:35:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-# Installing collected packages: watchdog, tzdata, typing-extensions, tornado, toml, tenacity, smmap, pyarrow, protobuf, numpy, narwhals, cachetools, pydeck, pandas, gitdb, altair, gitpython, streamlit
-
-
 import streamlit as st
 import json
 from datetime import date
@@ -10,17 +7,11 @@ from dateutil.relativedelta import relativedelta
 # Calculate the date 75 years ago from today
 
 min_date = date.today() - relativedelta(years=100)
-
 pages = {
-
     "Your account": [
-
         st.Page("pages/1_screening.py", title="Test App"),
-
         st.Page("pages/2_test_app.py", title="Nope")
-
     ]
-
 }
 
 pg = st.navigation(pages, position="top")
@@ -30,25 +21,16 @@ pg.run()
 # --- Page Configuration ---
 
 st.set_page_config(
-
     page_title="JSON Generator",
-
     layout="wide"  # Use wide layout for better column view
-
 )
 
 # pg = st.navigation(pages, position="top")
 
 st.markdown("""
-
     <style>
-
     .block-container {
-
         padding-top: 2rem;
-
     }
-
     </style>
-
     """, unsafe_allow_html=True)
