c608e098958c35cb6ff8f306397166c1bda19b9b
stevenstaley <73314563+stevenstaley@users.noreply.github.com>
2026-03-22 20:45:22 -0400

    Update 2_spot.py

--- pages/2_spot.py
+++ pages/2_spot.py
@@ -34,111 +34,14 @@ if 'generated_json' not in st.session_state:
     st.session_state.generated_json = None
 
 # --- Create Columns ---
-col1, col2, col3, col4, col5 = st.columns(5)
+col1, col2, col3 = st.columns(3)
 
 # --- Column 1: Data Entry ---
 with col1:
-    with st.container(border=True):
-        screener = st.text_input("Screener")
-        screendtg = st.datetime_input("Screening Date", value="now")
-    with st.container(border=True):
-        status = st.selectbox(
-        "Status",
-        ["Military", "Civilian", "Paramilitary", "Unknown"], index=None, placeholder="Select EPW Status"
-    )
-    with st.container(border=True):
-        st.subheader("Capture Data")
-        captdtg = st.datetime_input("Capture Date")
-        isn = st.text_input("Capture Tag/ISN")
-        pocapture = st.text_input("Place of Capture")
-        capture_unit = st.text_input("Capturing Unit")
-        capture_circ = st.text_area("Capture Circumstances")
-        docs = st.checkbox("Documents?")
-        docs_bool = False
-        if docs:
-            docdescription = st.text_area("Describe Documents")
-            docs_bool = True
-        else:
-            docdescription = ""
-        weapons = st.checkbox("Weapons?")
-        weapons_bool = False
-        if weapons:
-            wpnsdescription = st.text_area("List Weapons")
-            weapons_bool = True
-        else:
-            wpnsdescription = ""
-
-    # --- Section 1: Personal Info ---
+    
 with col2:
-    with st.container(border=True):
-        st.subheader("Personal Info")
-        fname = st.text_input("First Name")
-        lname = st.text_input("Last Name")
-        sex = st.selectbox(
-        "Sex",
-        ["Male", "Female", "Unknown"], index=None
-    )
-        dob = st.date_input("DOB", min_value=min_date)
-        pob = st.text_input("POB")
-        maritalstatus = st.selectbox(
-        "Marital Status",
-        ["Single", "Married", "Divorced", "Separated", "Widowed"], index=None
-    )
-    with st.container(border=True):
-        st.subheader("Civilian")
-        job = st.text_input("Job")
-        org = st.text_input("Organization")
-        duties = st.text_input("Duties")
-        skills = st.text_input("Skills")
-
-# --- Column 2: JSON Viewer ---
 
 with col3:
-    with st.container(border=True):
-        st.subheader("Military")
-        full_unit = st.text_input("Full Unit Designation")
-        dty_psn = st.text_input("Duty Position")
-        miljob = st.text_input("Military Job")
-        branch = st.selectbox(
-        "Branch",
-        ["Army", "Air Force", "Navy", "Marine Corps", "Coast Guard", "Space", "Other"], index=None, placeholder="Select Military Branch")
-        svcid = st.text_input("Service ID No")
-        station = st.text_input("Station")
-        milskills = st.text_input("Military Skills")
-        milexp = st.text_input("Military Experience")
-
-with col4:
-    with st.container(border=True):
-        st.subheader("Assesment Data")
-        wounded = st.selectbox(
-        "Wounded?",
-        ["No", "Yes"], index=None
-    )
-        intelligencelevel = st.selectbox(
-        "Intelligence Level",
-        ["AVG", "AVG-", "AVG+"], index=None
-    )
-        remarks = st.text_area("Remarks")
-        education = st.text_input("Education")
-        mentcondition = st.text_input("Mental Condition")
-        cooperation = st.select_slider(
-        "Cooperation",
-        options=[
-            1, 2, 3,
-        ],
-    )
-        knowledge = st.select_slider(
-        "Knowledge",
-        options=[
-            "A", "B", "C"
-        ],
-    )
-        assessment = st.text_area("Screener Assessment")
-        approach = st.text_area("Approach Recommendations")
-
-    # --- Generate Button ---
-
-with col5:
     if st.button("Generate JSON", type="primary"):
     # Create a Python dictionary from the input values
         output_data = {
