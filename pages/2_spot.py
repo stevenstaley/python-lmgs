70f7faedc24d298713091b9652594090d982f76b
stevenstaley <73314563+stevenstaley@users.noreply.github.com>
2026-03-23 18:32:50 -0400

    Update 2_spot.py

--- pages/2_spot.py
+++ pages/2_spot.py
@@ -3,6 +3,8 @@ import json
 from datetime import date
 import os
 from dateutil.relativedelta import relativedelta
+from utilities.functions import get_options_from_file
+from utilities import options
 
 min_date = date.today() - relativedelta(years=100)
 
@@ -10,7 +12,7 @@ st.set_page_config(
     page_title="SPOT",
     layout="wide"  # Use wide layout for better column view
 )
-
+team_options = get_options_from_file(options.TEAMS)
 # pg = st.navigation(pages, position="top")
 
 st.markdown("""
@@ -39,28 +41,31 @@ col1, col2 = st.columns(2)
 # --- Column 1: Data Entry ---
 with col1:
     with st.container(border=True):
+        team = st.selectbox("Team", options=team_options)
         who = st.text_input("Who")
         what = st.text_input("What")
-        when = st.datetime_input("When")
+        when = st.datetime_input("When", )
         where = st.text_input("Where")
         why = st.text_input("Why")
         how = st.text_area("How")
         other = st.text_area("Additional Information")
-
+    email_body = f"""Team: {team}
+Who: {who}
+What: {what}
+When: {when}
+Where: {where}
+Why: {why}
+How: {how}
+Additional Information: {other}
+"""
 with col2:
-    st.write(f"""
-        Who: {who}
-        What: {what}
-        When: {when}
-        Where: {where}
-        Why: {why}
-        How: {how}
-        Other: {other}
-        """)
+    st.text_area("Text for Email", email_body, height=300)
+
     if st.button("Generate JSON", type="primary"):
     # Create a Python dictionary from the input values
         output_data = {
             "report_type": "SPOT",
+            "team": team,
             "who": who,
             "what": what,
             "when": when,
@@ -80,8 +85,8 @@ with col2:
     else:
         generated_data = st.empty()
     if st.button("Save JSON to file"):
-        folder_path = "screenings/" + str(date.today())
-        file_name = isn + "-" + fname + "-" + lname + str(captdtg) + "-" + screener
+        folder_path = "spots/" + str(date.today())
+        file_name = "SPOT" + "-" + str(when)
         file_path = os.path.join(folder_path, f"{file_name}.json")
         # Create folder if it doesn't exist
         if not os.path.exists(folder_path):
@@ -99,4 +104,3 @@ with col2:
             stcode = st.code(json.dumps(generated_data, indent=4, default=str), language='json')
         else:
             stcode = st.empty()
-
