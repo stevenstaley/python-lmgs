cdbd41d14658845774821ed44f64c29b0db39fb7
stevenstaley <73314563+stevenstaley@users.noreply.github.com>
2026-03-22 20:56:58 -0400

    Update 2_spot.py

--- pages/2_spot.py
+++ pages/2_spot.py
@@ -34,77 +34,33 @@ if 'generated_json' not in st.session_state:
     st.session_state.generated_json = None
 
 # --- Create Columns ---
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 # --- Column 1: Data Entry ---
 with col1:
     with st.container(border=True):
-        captdtg = st.datetime_input("Capture Date")
         who = st.text_input("Who")
         what = st.text_input("What")
-        who = st.text_input("Who")
-        who = st.text_input("Who")
-        who = st.text_input("Wh")
-with col2:
+        when = st.datetime_input("When")
+        where = st.text_input("Where")
+        why = st.text_input("Why")
+        how = st.text_area("How")
+        other = st.text_area("Additional Information")
 
-with col3:
+with col2:
     if st.button("Generate JSON", type="primary"):
     # Create a Python dictionary from the input values
         output_data = {
-            "screener": screener,
-            "screening_date": screendtg,
-            "status": status,
-            "capture_data": {
-                "capture_tag_isn": isn,
-                "capture_date": captdtg,
-                "place_of_capture": pocapture,
-                "capturing_unit": capture_unit,
-                "capture_circumstances": capture_circ,
-                "documents": {
-                    "docsyesorno": docs_bool,
-                    "documents_description": docdescription
-                },
-                "weapons": {
-                    "wpnsyesorno": weapons_bool,
-                    "weapons_description": wpnsdescription
-                },
-            "personal_info": {
-                "first_name": fname,
-                "last_name": lname,
-                "sex": sex,
-                "dob": dob,
-                "pob": pob,
-                "marital_status": maritalstatus
-            },
-            "military": {
-                "full_unit_designation": full_unit,
-                "duty_position": dty_psn,
-                "job": miljob,
-                "branch": branch,
-                "service_id": svcid,
-                "station": station,
-                "skills": milskills,
-                "military_experience": milexp
-            },
-            "civilian": {
-                "job": job,
-                "organization": org,
-                "duties": duties,
-                "skills": skills
-            },
-            "assessment_data": {
-                "wounded": wounded,
-                "intelligence_level": intelligencelevel,
-                "remarks": remarks,
-                "education": education,
-                "mental_condition": mentcondition,
-                "cooperation": cooperation,
-                "knowledge": knowledge,
-                "screener_assessment": assessment,
-                "approach_recommendations": approach
-            }
-    }
-    }
+            "report_type": "SPOT",
+            "who": who,
+            "what": what,
+            "when": when,
+            "where": where,
+            "why": why,
+            "how": how,
+            "additional_information": other
+        }
+
         # Store the generated data in the session state
 
         st.session_state.generated_json = output_data
