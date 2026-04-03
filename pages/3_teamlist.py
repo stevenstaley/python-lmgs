e3feecf3d74b1d6f9630961e04e8e00bfcf35d6c
Steven Staley <steven.w.staley2@gmail.com>
2026-04-03 09:38:29 -0400

    updated teamlit

--- pages/3_teamlist.py
+++ pages/3_teamlist.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utilities.functions import load_options, save_options
 from utilities.configuration import configure_page
-from utilities.options import TEAMS, PERSONNEL
+from utilities.options import TEAMS, PERSONNEL, RNS
 
 configure_page("Teams and Personnel List")
 
@@ -11,7 +11,7 @@ st.title("Teams and Personnel")
 
 
 # --- Page Content ---
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     # Load existing options to pre-fill the text area
@@ -47,4 +47,22 @@ with col2:
         # Save the current content of the text area to the file
         save_options(text_options1, PERSONNEL)
         st.success(f"✅ Your dropdown options have been saved to `{PERSONNEL}`!")
+        st.info("You can now go to the 'Main Page' to see the updated dropdown.")
+
+with col3:
+    # Load existing options to pre-fill the text area
+    existing_options_text2 = load_options(RNS)
+    
+    # Text area for user to input/edit the options
+    text_options2 = st.text_area(
+        "Reporter Numbers",
+        value=existing_options_text2,
+        height=300,
+        help="Enter each dropdown option on a new line. You can add, edit, or remove options below. Each line will become a separate option in the dropdown list."
+    )
+    
+    if st.button("Save Reporter Numbers"):
+        # Save the current content of the text area to the file
+        save_options(text_options2, RNS)
+        st.success(f"✅ Your dropdown options have been saved to `{RNS}`!")
         st.info("You can now go to the 'Main Page' to see the updated dropdown.")
